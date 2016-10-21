21ba2d65ce018e754807efa6078692b76e6fb17c
Dan Pickett <dan.pickett@launchacademy.com>
2016-10-21 18:04:59 -0400

    integrate react along w/ simple example

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log('works');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Hello = React.createClass({
+  render: function() {
+    return <div>Hello {this.props.name}</div>;
+  }
+});
+
+ReactDOM.render(
+  <Hello name="World" />,
+  document.getElementById('container')
+);
